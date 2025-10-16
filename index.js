6835ed688529e32390714c2ac0ad94bfb6ccdbce
Kingsley <kifeanyi216@gmail.com>
2025-10-16 14:44:27 +0100

    DB Connection

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import pool from "./src/config/db.js";
 
 dotenv.config();
 
@@ -14,6 +15,12 @@ app.use(cors());
 // Routes
 
 // Error handling middleware
+
+// Testing POSTGRES connection
+app.get("/", async (req, res) => {
+  const result = await pool.query("SELECT current_database()");
+  res.send(`The database name is : ${result.rows[0].current_database}`);
+});
 
 // Start the server
 app.listen(port, () => {
